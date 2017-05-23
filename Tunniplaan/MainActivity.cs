31cf5b16a25f7f675c23a153481a661a0a1821dc
Kes siis mu kui Matu <Kes siis mu kui Matu@DESKTOP-CSIQTVF>
2017-05-23 17:41:57 +0300

    Add project files.

---
+++ Tunniplaan/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+using HtmlAgilityPack;
+using System;
+using AngleSharp;
+
+namespace Tunniplaan
+{
+    [Activity(Label = "Tunniplaan", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        WebView webView;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            ryhmadList();
+          
+            SetContentView(Resource.Layout.Main);
+            webView = FindViewById<WebView>(Resource.Id.webView1);
+            webView.SetWebViewClient(new WebViewClient());
+            webView.Settings.JavaScriptEnabled = true;
+            Button g = FindViewById<Button>(Resource.Id.grupid);
+            
+
+            string data = "<body>" + "<img src=\"http://www.tptlive.ee/wp-content/uploads/2014/07/tpt_logo2.png\"/> </body>";
+
+            webView.LoadData(data, "text/html", "UTF-8");
+            // webView.LoadUrl("http://www.tptlive.ee/wp-content/uploads/2014/07/tpt_logo2.png");
+            g.Click += delegate
+            {
+                webView.LoadUrl("https://tpt.siseveeb.ee/veebivormid/kiosk/tunniplaan?tyyp=grupid");
+            };
+
+      
+
+
+
+
+        }
+
+
+        public  void ryhmadList()
+        {
+            var document = new HtmlDocument();
+            document.LoadHtml("https://tpt.siseveeb.ee/veebivormid/kiosk/tunniplaan?tyyp=grupid");
+    
+        }
+    }
+    
+}
+
